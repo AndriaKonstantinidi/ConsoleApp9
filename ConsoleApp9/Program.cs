240b9e762e56a2bc020d25264c3cb07a28432cbf
visual studio <vs@itstep.ge>
2023-10-24 18:53:17 +0400

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,42 @@
+﻿//using ConsoleApp9;
+
+//string[] transportType = new string[] { "air", "sea", "land" };
+
+//List<Transport> transports = new List<Transport>
+//{
+//    new Transport(){TransportType = "land", TransportName = "Train"},
+//    new Transport(){TransportType = "land", TransportName = "Car"},
+//    new Transport(){TransportType = "land", TransportName = "Bus"},
+//    new Transport(){TransportType = "land", TransportName = "Metro"},
+//    new Transport(){TransportType = "sea", TransportName = "Boat"},
+//    new Transport(){TransportType = "sea", TransportName = "Cargo Ship"},
+//    new Transport(){TransportType = "air", TransportName = "Helicopter"},
+//    new Transport(){TransportType = "air", TransportName = "Boeing"},
+//};
+
+
+//var groupJoin = from tt in transportType
+//                join tr in transports
+//                on tt equals tr.TransportType
+//                into transportGroup
+//                select new
+//                {
+//                    type = tt,
+//                    transports = transportGroup
+//                };
+
+//foreach(var item in groupJoin)
+//{
+//    Console.WriteLine(item.type);
+//    foreach(var i in item.transports)
+//    {
+//        Console.WriteLine($"\t {i.TransportName}");
+//    }
+//}
+
+
+//int[] nums = { 1, -2, 3, 0, 4 ,5 };
+
+//var query = nums.Where(i=> i > 0).ToList();
+
+//query.ForEach(query => Console.WriteLine(query));
---
+++ ConsoleApp9/Transport.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp9
+//{
+//    internal class Transport
+//    {
+//        public string TransportType { get; set; }
+//        public string TransportName { get; set; }
+//    }
+//}
